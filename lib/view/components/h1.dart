0dfc42a6a11018b6c26055f09ccdbd406d968942
mvgarc <magarcster@gmail.com>
2024-11-04 10:56:42 -0400

    updated: para que pueda cambiar de color

--- lib/view/components/h1.dart
+++ lib/view/components/h1.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 class H1 extends StatelessWidget {
-  const H1 (this.text,{super.key});
+  const H1 (this.text,{super.key, this.color,});
 
   final String text;
   final Color? color;
