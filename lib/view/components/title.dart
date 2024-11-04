cf79f4c6c1c930042b4b93ee66e12cf7c51b5490
mvgarc <magarcster@gmail.com>
2024-11-04 10:21:24 -0400

    updated

--- lib/view/components/title.dart
+++ lib/view/components/title.dart
@@ -1,9 +1,14 @@
 
-class  extends StatelessWidget {
-  const ({super.key});
+import 'package:flutter/material.dart';
+
+class Title extends StatelessWidget {
+  const Title({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => Text('Mensajes bonitos para tía',
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+          fontSize: 18,
+      fontWeight: FontWeight.w600,
+    ),
+    );
 }
