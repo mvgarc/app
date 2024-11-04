f4230d93fb30a6eef434d8b2a637963720441518
mvgarc <magarcster@gmail.com>
2024-11-04 19:09:36 -0400

    updated

--- lib/view/task_list/task_list.dart
+++ lib/view/task_list/task_list.dart
@@ -8,63 +8,77 @@ class TaskListPage extends StatefulWidget {
 }
 
 class _TaskListPageState extends State<TaskListPage> {
-  int count = 0;
-  final TextEditingController _controller = TextEditingController();
-  final List<String> _messages = [];
+  final TextEditingController _searchController = TextEditingController();
+  final List<String> _allImages = [
+    'https://www.kissfm.es/wp-content/uploads/2022/03/piolin-2000x838.jpg',
+    'https://i.pinimg.com/474x/52/40/7b/52407b651f189e08e699dfcffe41105e.jpg',
+    'https://i.pinimg.com/564x/ed/00/f2/ed00f23259c3e4f8acee84e67106bc56.jpg',
+    'https://i.pinimg.com/564x/ed/00/f2/ed00f23259c3e4f8acee84e67106bc56.jpg',
+    'https://i.pinimg.com/564x/ed/00/f2/ed00f23259c3e4f8acee84e67106bc56.jpg',
+    'https://i.pinimg.com/564x/ed/00/f2/ed00f23259c3e4f8acee84e67106bc56.jpg',
+    // Agrega más URLs o usa imágenes locales
+  ];
+  List<String> _filteredImages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredImages = List.from(_allImages); // Mostrar todas las imágenes inicialmente
+  }
+
+  void _filterImages(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredImages = List.from(_allImages);
+      } else {
+        _filteredImages = _allImages.where((image) => image.contains(query)).toList();
+      }
+    });
+  }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _searchController.dispose();
     super.dispose();
-  }
-
-  void _addMessage() {
-    setState(() {
-      if (_controller.text.isNotEmpty) {
-        _messages.add(_controller.text);
-        _controller.clear(); // Limpiar el campo después de guardar
-      }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mensajes'),
+        title: Text('Galería de Fotos'),
       ),
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
-            child: Text('Crea tu mensaje'),
-          ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            padding: const EdgeInsets.all(16.0),
             child: TextField(
-              controller: _controller,
+              controller: _searchController,
+              onChanged: _filterImages,
               decoration: InputDecoration(
-                labelText: 'Escribe tu mensaje',
+                labelText: 'Buscar imagen',
                 border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
               ),
             ),
           ),
-          SizedBox(height: 10),
           Expanded(
-            child: ListView.builder(
-              itemCount: _messages.length,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // Número de columnas en la galería
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,
+              ),
+              itemCount: _filteredImages.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_messages[index]),
+                return Image.network(
+                  _filteredImages[index],
+                  fit: BoxFit.cover,
                 );
               },
             ),
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addMessage,
-        child: Icon(Icons.add),
       ),
     );
   }
